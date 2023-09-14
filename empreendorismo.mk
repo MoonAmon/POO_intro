4b7d5a6b4a0a454a930f9a9e6a0c4cc4ed1aa984
MoonAmon <misterofclans.mo@gmail.com>
2023-09-14 20:38:21 -0300

    atv swift empreendorismo

---
+++ empreendorismo.mk
@@ -0,0 +1,5 @@
+aplicativo de gamificação de educação 
+publico alvo: universitarios, alunos
+idade: todas as idades
+
+tem como objetivo gamificar os estudos de acordo com oq o aluno esta estudando, com xp e niveis, para facilitar a criação de habito de estudos com um habit tracker, com elementos que deixe bem aparente a constancia....
